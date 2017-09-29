6c4bffbe8abae7b08f64c3bf87a213601ace9b7e
littleflute <13699175164@sina.cn>
2017-09-29 11:57:47 +0800

    Update Index.js

--- ref/pu/Index.js
+++ ref/pu/Index.js
@@ -1,13 +1,17 @@
-var v="v0.0.6";
+var v="v0.0.7";
 var n = 0;
 n++;
 var s = "p";
 s += n;
 s += ".txt";
+function parsMoveList(param)
+{
+            alert(param);
+}
 $("#myPu").load(s, function(responseTxt, statusTxt, xhr){
             if(statusTxt == "success")
-            {
-                alert("External content loaded successfully!");
+            { 
+                parsMoveList(responseTxt);
             }
             if(statusTxt == "error")
             {
