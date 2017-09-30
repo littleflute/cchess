1111290af05c6fe469997f3025387a629b05d19f
littleflute <13699175164@sina.cn>
2017-09-30 08:30:39 +0800

    Update blToolBar.js

--- ref/DhtmlXQ_www_dpxq_com/blToolBar.js
+++ ref/DhtmlXQ_www_dpxq_com/blToolBar.js
@@ -1,5 +1,11 @@
-var v_blToolBar = "v0.0.1";
+var v_blToolBar = "v0.0.2";
 var xdUI 	= document.getElementById("xdUI");
+var jd  = runJSDiv;
 
-var tb1 = runJSDiv.blDiv(xdUI,"ToolBar1","TB1:");
+var tb1 = jd.blDiv(xdUI,"ToolBar1","TB1:");
 tb1.style.backgroundColor = "green";
+var btn1 = jd.blBtn(tb1,"btn1","btn1");
+btn1.onclick = function(){
+  	var href = "https://littleflute.github.io/blog/js/blDraggableDIV.js";
+    jd.loadJS2TA(href);	
+}
