2ed05bc06ef77621003ee026e4d79b38b01513c4
littleflute <13699175164@sina.cn>
2017-09-30 13:31:12 +0800

    Update blToolBar.js

--- ref/DhtmlXQ_www_dpxq_com/blToolBar.js
+++ ref/DhtmlXQ_www_dpxq_com/blToolBar.js
@@ -1,4 +1,4 @@
-var v_blToolBar = "v0.0.9";
+var v_blToolBar = "v0.1.0";
 var xdUI 	= document.getElementById("xdUI");
 var jd  = runJSDiv;
 
@@ -6,6 +6,6 @@ var tb1 = jd.blDiv(xdUI,"ToolBar1","Real:");
 tb1.style.backgroundColor = "green";
 var btnReal1 = jd.blBtn(tb1,"real1","real1");
 btnReal1.onclick = function(){
-  	var href = "https://littleflute.github.io/chess/ref/DhtmlXQ_www_dpxq_com/real/1.txt";
+  	var href = "https://littleflute.github.io/cchess/ref/DhtmlXQ_www_dpxq_com/real/1.txt";
     jd.loadJS2TA(href);	
 }
